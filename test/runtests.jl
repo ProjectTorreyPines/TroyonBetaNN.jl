71b2748ef289df03e26d73beb792291b3402a5bd
Min-Gu Yoo <mgyoo86@gmail.com>
2024-10-22 14:37:17 -0700

    Update runtests to show simple terminal output

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,13 @@ using IMAS
 
     file_path = joinpath(@__DIR__, "data", "dd_D3D.json")
     dd = IMAS.json2imas(file_path);
+
+    # run & show simple terminal output
+    TD_vec = TBNN.Calculate_Troyon_beta_limits_for_IMAS_dd(dd);
+
+    # run & show verbose terminal output
     TD_vec = TBNN.Calculate_Troyon_beta_limits_for_IMAS_dd(dd; verbose=true);
+
     TD=TD_vec[2]
     @testset "Check MLP's βₙ_limits with references" begin
         @test TD.MLPs[1].βₙ_limit ≈ 3.5083038793808123 atol=1e-6
