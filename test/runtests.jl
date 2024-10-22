0eab5a48f0d475a8f0588d80d3aa09b8bf647270
Min-Gu Yoo <mgyoo86@gmail.com>
2024-10-22 10:47:59 -0700

    testcase for (dd_D3D.json)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
-using TroyonBetaNN
 using Test
 
+import TroyonBetaNN as TBNN
+using IMAS
+
 @testset "TroyonBetaNN.jl" begin
-    # Write your tests here.
+
+    file_path = joinpath(@__DIR__, "data", "dd_D3D.json")
+    dd = IMAS.json2imas(file_path);
+    TD_vec = TBNN.Calculate_Troyon_beta_limits_for_IMAS_dd(dd; verbose=true);
+    TD=TD_vec[2]
+    @testset "Check MLP's βₙ_limits with references" begin
+        @test TD.MLPs[1].βₙ_limit ≈ 3.5083038793808123 atol=1e-6
+        @test TD.MLPs[2].βₙ_limit ≈ 3.521892957155812 atol=1e-6
+        @test TD.MLPs[3].βₙ_limit ≈ 3.9271056622431315 atol=1e-6
+    end
+
 end
