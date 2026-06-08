2b37f16e20d3974756e1ff24655c72090fc9652e
Min-Gu Yoo <mgyoo86@gmail.com>
2026-06-08 10:08:35 -0700

    refactor: update reference values and tolerances in tests for FastInterpolations

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,16 +22,16 @@ include("references.jl")
 
     TD = TD_vec[2]
     @testset "Check MLP's βₙ_limits with references" begin
-        @test isapprox(TD.MLPs[1].βₙ_limit, 3.5083038793808123; atol=1e-6)
-        @test isapprox(TD.MLPs[2].βₙ_limit, 3.521892957155812; atol=1e-6)
-        @test isapprox(TD.MLPs[3].βₙ_limit, 3.9271056622431315; atol=1e-6)
+        @test isapprox(TD.MLPs[1].βₙ_limit, 3.5040075164972104; rtol=1e-3)
+        @test isapprox(TD.MLPs[2].βₙ_limit, 3.5221171764974266; rtol=1e-3)
+        @test isapprox(TD.MLPs[3].βₙ_limit, 3.9239197016232703; rtol=1e-3)
     end
 
     @testset "Interpolation outputs (sample points)" begin
-        @test isapprox(TD.sampPoints.q,        Q_REF;        rtol=1e-12)
-        @test isapprox(TD.sampPoints.pressure, PRESSURE_REF; rtol=1e-12)
-        @test isapprox(TD.sampPoints.R,        R_REF;        rtol=1e-12)
-        @test isapprox(TD.sampPoints.Z,        Z_REF;        rtol=1e-12)
+        @test isapprox(TD.sampPoints.q,        Q_REF;        rtol=1e-3)
+        @test isapprox(TD.sampPoints.pressure, PRESSURE_REF; rtol=1e-3)
+        @test isapprox(TD.sampPoints.R,        R_REF;        rtol=1e-3)
+        @test isapprox(TD.sampPoints.Z,        Z_REF;        rtol=1e-3)
     end
 
 end
